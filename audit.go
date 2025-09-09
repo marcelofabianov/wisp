a69e993621a56321f44b1e1e9b3aaf525f2ac137
Marcelo Fabiano <marcelofabianov@gmail.com>
2025-09-09 13:08:04 -0300

    feat: criar struct audit

---
+++ audit.go
@@ -0,0 +1,11 @@
+package atomic
+
+type Audit struct {
+	CreatedAt  CreatedAt    `db:"audit_created_at" json:"created_at"`
+	CreatedBy  AuditUser    `db:"audit_created_by" json:"created_by"`
+	UpdatedAt  UpdatedAt    `db:"audit_updated_at" json:"updated_at"`
+	UpdatedBy  AuditUser    `db:"audit_updated_by" json:"updated_by"`
+	ArchivedAt NullableTime `db:"audit_archived_at" json:"archived_at,omitempty"`
+	DeletedAt  NullableTime `db:"audit_deleted_at" json:"deleted_at,omitempty"`
+	Version    Version      `db:"audit_version" json:"version"`
+}
